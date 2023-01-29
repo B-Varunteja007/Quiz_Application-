a6a27b2e574777f1d79b31e67aa0d393118e0d08
B-Varunteja007 <105963221+B-Varunteja007@users.noreply.github.com>
2023-01-29 10:44:18 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which	muscle	is	inserted	into	the	floor	of the	intertubercular	sulcus	of	the	humerus?",
+    answer: "Latissimus dorsi",
+    options: [
+      "Latissimus dorsi",
+      "Teres major",
+      "Pectoralis major",
+      "Deltoid"
+    ]
+  },
+    {
+    numb: 2,
+    question: "At what level does the trachea bifurcates?",
+    answer: "Lower border of T4",
+    options: [
+      "Upper border	of T4",
+      "Lower border of T4",
+      "27.5cm from the incisors",
+      "Lower border	of T5"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Skeletal derivative	of 2°' pharyngeal arch-",
+    answer: "Stapes",
+    options: [
+      "Malleus",
+      "Incus",
+      "Stapes",
+      "Maxilla"
+    ]
+  },
+    {
+    numb: 4,
+    question: "All are derived from ectoderm except-",
+    answer: "Adrenal cortex",
+    options: [
+      "Hypophysis",
+      "Retina",
+      "Spinal cord",
+      "Adrenal cortex"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Blastocyst makes	contact	with endometrium on?",
+    answer: "5-7 days",
+    options: [
+      "<3 days",
+      "5-7 days",
+      "8-11 days",
+      "15-16 days"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
